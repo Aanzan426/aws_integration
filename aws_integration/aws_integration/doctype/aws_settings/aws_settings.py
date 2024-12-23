e098d2dbecfe126ac51ddc77de5c1a104b02942c
badal8381 <badalsahani8381@gmail.com>
2024-12-23 14:06:47 +0530

    Implement AWS SES email sending with rate limit

---
+++ aws_integration/aws_integration/doctype/aws_settings/aws_settings.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2024, Hybrowlabs Technologies and contributors
+# For license information, please see license.txt
+import boto3
+import frappe
+from frappe import _
+from botocore.exceptions import ClientError
+from frappe.model.document import Document
+from aws_integration.utils import validate_email
+
+
+class AWSSettings(Document):
+
+    def get_ses_client(self):
+        aws_secret_access_key = self.get_password("aws_secret_access_key")
+        return boto3.client(
+            "sesv2",
+            region_name=self.region,
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+        )
+
+    def validate(self):
+        if self.source_email and not validate_email(self.source_email):
+            frappe.throw("Please enter valid email address")
+
+
+    def send_email(
+        self,
+        destinations,
+        subject,
+        content=None,
+        html=None,
+        reply_tos=None,
+    ):
+        """
+        Sends emails in batches with a rate limit of 25 per second.
+
+        :param destinations: List of destinations (objects with `to_service_format` method).
+        :param subject: Email subject.
+        :param text: Plain text body (optional).
+        :param html: HTML body (optional).
+        :param reply_tos: List of reply-to addresses (optional).
+        :param batch_size: Number of recipients per batch (default: 25).
+        :return: List of message IDs or None for failed attempts.
+        """
+        self.source = f"{self.sender_name} <{self.source_email}>"
+        self.ses_client = self.get_ses_client()
+        send_args = {
+            "FromEmailAddress": self.source,
+            "Destination": destinations.to_service_format(),
+            "Content": {
+                "Simple": {
+                    "Subject": {"Data": subject, "Charset": "UTF-8"},
+                    "Body": {},
+                }
+            },
+        }
+
+        if content:
+            send_args["Content"]["Simple"]["Body"]["Text"] = {
+                "Data": content,
+                "Charset": "UTF-8",
+            }
+
+        if html:
+            send_args["Content"]["Simple"]["Body"]["Html"] = {
+                "Data": html,
+                "Charset": "UTF-8",
+            }
+
+        if reply_tos:
+            send_args["ReplyToAddresses"] = reply_tos
+
+        try:
+            response = self.ses_client.send_email(**send_args)
+            return response
+        except ClientError as e:
+            frappe.log_error(
+                _("Failed to send email: {error}").format(error=str(e)),
+                frappe.get_traceback(),
+            )
---
+++ aws_integration/utils/__init__.py
@@ -0,0 +1,96 @@
+import re
+import time
+import frappe
+from frappe import _, cint
+from itertools import islice
+
+
+class SESDestination:
+    """Contains data about an email destination."""
+
+    def __init__(self, tos, ccs=None, bccs=None):
+        self.tos = tos
+        self.ccs = ccs
+        self.bccs = bccs
+
+    def to_service_format(self):
+        svc_format = {"ToAddresses": self.tos}
+        if self.ccs:
+            svc_format["CcAddresses"] = self.ccs
+        if self.bccs:
+            svc_format["BccAddresses"] = self.bccs
+        return svc_format
+
+
+def validate_email(email):
+    """Regular expression to validate email addresses."""
+    email_pattern = re.compile(r"[^@]+@[^@]+\.[^@]+")
+    return bool(email_pattern.match(email))
+
+
+def is_html(text):
+    """Regular expression to check for HTML tags"""
+    html_pattern = re.compile(r"<([a-zA-Z]+)[^>]*>(.*?)</\1>|<([a-zA-Z]+)[^>]*>")
+    return bool(html_pattern.search(text))
+
+
+def chunk(iterable, size):
+    """Yield successive chunks of a specified size from an iterable."""
+    iterator = iter(iterable)
+    for first in iterator:
+        yield [first, *islice(iterator, size - 1)]
+
+
+def sendmail(subject, message, recepient, cc_recepient, bcc_recepient, reply_tos=None):
+    email_sender = frappe.get_single("AWS Settings")
+    destinations = SESDestination(tos=recepient, ccs=cc_recepient, bccs=bcc_recepient)
+
+    email_params = {
+        "destinations": destinations,
+        "subject": subject,
+        "reply_tos": reply_tos,
+    }
+
+    if is_html(message):
+        email_params["html"] = message
+    else:
+        email_params["content"] = message
+
+    response = email_sender.send_email(**email_params)
+
+    message_id = response.get("MessageId")
+    if not message_id:
+        frappe.throw(_("Failed to send email. Please try again."))
+
+    return message_id
+
+
+def send_email_in_batches(data):
+    """
+    Structure of data:
+    {
+        "key_name": {
+            "subject": "Subject",
+            "content": "Content",
+            "recepients": [],
+            "cc_recepients": [],
+            "bcc_recepients": [],
+            reply_tos: []
+        }
+    }
+    """
+    email_batch_size = frappe.get_value(
+        "AWS Settings", "AWS Settings", "email_batch_size"
+    )
+
+    for student_data in chunk(data.keys(), cint(email_batch_size)):
+        for key in student_data:
+            student = data[key]
+            sendmail(
+                student.get("subject"),
+                student.get("content"),
+                student.get("recepients"),
+                student.get("cc_recepients"),
+                student.get("bcc_recepients"),
+            )
+        time.sleep(1)
